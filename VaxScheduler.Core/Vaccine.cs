6a93dd10f278b918e337b0e80346478db2f408a6
yaseenhamdy <yaseenhamdy74@gmail.com>
2024-04-19 23:24:20 +0200

    Add Vaccine Class

---
+++ VaxScheduler.Core/Vaccine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaxScheduler.Core
+{
+	internal class Vaccine
+	{
+        public int Id { get; set; }
+    }
+}
